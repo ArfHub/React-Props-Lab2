d9a9956f69387e66c623a55e994cb207b2c226d5
ARFHSI <Abdulraouf.hsi@gmail.com>
2024-06-09 20:23:06 +0300

    W7-D1-Lab2

---
+++ props-lab2/src/App.jsx
@@ -0,0 +1,36 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './component/Card'
+import Nav from './component/Nav'
+import Foot from './component/Footer'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <Nav />
+      <br />
+
+      <h1>wounderful wallpaper</h1>
+      <hr />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+
+      <Foot />
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
---
+++ props-lab2/src/component/Card.jsx
@@ -0,0 +1,45 @@
+function Card(props){
+    return (
+
+<div class="card-group">
+  <div class="card">
+    <img src="https://images.pexels.com/photos/21403982/pexels-photo-21403982/free-photo-of-black-and-white-photograph-of-flowers-in-a-vase.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">white flowers</h5>
+      <p class="card-text">it has a great smell</p>
+      <ul>
+      <li>found in Ksa</li>
+      <li>has a special perfume </li>
+      <li> grows only in winter</li>
+      </ul>
+    </div>
+  </div>
+  <div class="card">
+    <img src="https://images.pexels.com/photos/15511422/pexels-photo-15511422/free-photo-of-full-moon-over-birds-silhouettes-on-tree.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">moonlight</h5>
+      <p class="card-text">we all like to set under moonlight</p>
+      <ul>
+      <li>comes only in the night</li>
+      <li>we can know about prayer time </li>
+      <li>we can know about fasting time</li>
+      </ul>
+    </div>
+  </div>
+  <div class="card">
+    <img src="https://images.pexels.com/photos/24491300/pexels-photo-24491300/free-photo-of-a-small-house-sits-in-the-middle-of-a-field.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">home</h5>
+      <p class="card-text">home sweet home</p>
+      <ul>
+      <li>where family always gatharing</li>
+      <li>where we feel safe </li>
+      <li>where we born</li>
+      </ul>
+    </div>
+  </div>
+</div>
+      )
+}
+
+export default Card
